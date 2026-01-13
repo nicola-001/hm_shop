f692f9d40f904de30516758bee7f45774154c07e
nicola <2629137145@qq.com>
2026-01-13 10:43:55 +0800

    完成轮播图搜索框和指示灯导航

--- lib/components/Home/HmSlider.dart
+++ lib/components/Home/HmSlider.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:hm_shop/viewmodels/Home.dart';
 
 class HmSlider extends StatefulWidget {
-  //接收负组建传递过来的数据
+  //接收父组件传递过来的数据
   final List<BannerItem> bannerList;
 
   const HmSlider({super.key, required this.bannerList});
@@ -14,6 +14,12 @@ class HmSlider extends StatefulWidget {
 }
 
 class _HmSliderState extends State<HmSlider> {
+  // 定义controller
+  CarouselSliderController _controller = CarouselSliderController();
+
+  // 定义当前索引
+  int _currentIndex = 0;
+
   //构建轮播图
   Widget _getSlider() {
     //获取屏幕宽度
@@ -21,6 +27,7 @@ class _HmSliderState extends State<HmSlider> {
     //返回轮播图插件
     // 根据数据渲染的不同数据选项
     return CarouselSlider(
+      carouselController: _controller, // 绑定控制器-点击指示灯切换
       items: List.generate(widget.bannerList.length, (index) {
         return Image.network(
           widget.bannerList[index].imgUrl,
@@ -32,7 +39,71 @@ class _HmSliderState extends State<HmSlider> {
       options: CarouselOptions(
         autoPlayInterval: Duration(seconds: 5),
         viewportFraction: 1.0,
-        autoPlay: true,
+        autoPlay: true, //自动切换轮播图
+        onPageChanged: (index, reason) {
+          _currentIndex = index;
+          setState(() {});
+        },
+      ),
+    );
+  }
+
+  //构建搜索栏
+  Widget _getGetSearch() {
+    return Positioned(
+      top: 0,
+      left: 0,
+      right: 0,
+      child: Padding(
+        padding: EdgeInsetsGeometry.all(10),
+        child: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          height: 50,
+          decoration: BoxDecoration(
+            color: const Color.fromRGBO(0, 0, 0, 0.4),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          child: Text(
+            "搜索...",
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+        ),
+      ),
+    );
+  }
+
+  //构建指示灯导航
+  Widget _getDots() {
+    return Positioned(
+      left: 0,
+      right: 0,
+      bottom: 10,
+      child: SizedBox(
+        height: 40,
+        width: double.infinity,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(widget.bannerList.length, (int index) {
+            return GestureDetector(
+              onTap: () {
+                _controller.animateToPage(index);
+                index = index;
+                setState(() {});
+              },
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                height: 6,
+                width: index == _currentIndex ? 40 : 20,
+                margin: EdgeInsetsGeometry.symmetric(horizontal: 4),
+                decoration: BoxDecoration(
+                  color: index == _currentIndex ? Colors.white : Colors.white54,
+                  borderRadius: BorderRadius.circular(3),
+                ),
+              ),
+            );
+          }),
+        ),
       ),
     );
   }
@@ -40,6 +111,6 @@ class _HmSliderState extends State<HmSlider> {
   @override
   Widget build(BuildContext context) {
     //轮播图--搜索框--指示灯导航
-    return Stack(children: [_getSlider()]);
+    return Stack(children: [_getSlider(), _getGetSearch(), _getDots()]);
   }
 }
